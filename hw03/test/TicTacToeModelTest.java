25a14703f9fab5254d716883406273b93251c892
RoyBao96 <baoruyi.hc07@gmail.com>
2024-04-08 12:32:12 -0700

    update hw03

--- hw03/test/TicTacToeModelTest.java
+++ hw03/test/TicTacToeModelTest.java
@@ -1,26 +1,43 @@
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TicTacToeModelTest {
   private TicTacToeModel ticTacToeModel;
-  ticTacToeModel=new TicTacToeModelImp();
+  @Before
+  public void setup(){
+    ticTacToeModel=new TicTacToeModelImp();
+  }
+
 
   @Test
-  public void move() {
+  public void testmovetypical() {
     ticTacToeModel.move(0, 0);
     assertEquals(Player.X, ticTacToeModel.getMarkAt(0, 0));
+  }
+  @Test
+  public void testmoveexceptional() {
     //Attempt to move to an already occupied position should throw an exception
     ticTacToeModel.move(1, 1);
     assertThrows(IllegalStateException.class, () -> ticTacToeModel.move(1, 1));
-
   }
 
   @Test
-  public void nextPlayer() {
+  public void testnextPlayertypical() {
+    //Player X moves first
     ticTacToeModel.move(0, 0);
     assertEquals(Player.O, ticTacToeModel.nextPlayer());
+
+  }
+  @Test
+  public void testnextPlayerexceptional() {
+    /*
+     *    O   O
+     *    X   X   X
+     *            O
+     */
     ticTacToeModel.move(1, 1); // Player X moves
     ticTacToeModel.move(0, 0); // Player O moves
     ticTacToeModel.move(1, 2); // Player X moves
@@ -29,53 +46,97 @@ public class TicTacToeModelTest {
     ticTacToeModel.move(2, 2); // Player O moves
     //After a game over, calling NextPlayer should throw an exception
     assertThrows(IllegalStateException.class, () -> ticTacToeModel.nextPlayer());
+
   }
 
   @Test
-  public void getMarkAt() {
+  public void testgetMarkAttypical() {
+    ticTacToeModel.move(1, 1); // Player X moves
+    assertEquals(Player.X,ticTacToeModel.getMarkAt(1,1));
     assertNull(ticTacToeModel.getMarkAt(1, 2));
+
+  }
+  @Test
+  public void testgetMarkAtexceptional() {
     //mark out of bounds should throw an exception
     assertThrows(IndexOutOfBoundsException.class, () -> ticTacToeModel.getMarkAt(4, 4));
+
   }
 
   @Test
-  public void isGameOver() {
+  public void isGameOverwin() {
+
     //win
-    ticTacToeModel.move(0, 0);
-    ticTacToeModel.move(1, 0);
-    ticTacToeModel.move(0, 1);
-    ticTacToeModel.move(1, 1);
-    ticTacToeModel.move(0, 2);
+    /*
+     *    O   O
+     *    X   X   X
+     *            O
+     */
+    assertFalse(ticTacToeModel.isGameOver());
+    ticTacToeModel.move(1, 1); // Player X moves
+    ticTacToeModel.move(0, 0); // Player O moves
+    ticTacToeModel.move(1, 2); // Player X moves
+    ticTacToeModel.move(0, 1); // Player O moves
+    ticTacToeModel.move(1, 0); // Player X moves
+    ticTacToeModel.move(2, 2); // Player O moves
     assertTrue(ticTacToeModel.isGameOver());
+
+  }
+  @Test
+  public void isGameOvertie() {
     //tie
-    ticTacToeModel.move(0, 0);
-    ticTacToeModel.move(1, 0);
-    ticTacToeModel.move(2, 0);
-    ticTacToeModel.move(1, 1);
-    ticTacToeModel.move(0, 1);
-    ticTacToeModel.move(0, 2);
-    ticTacToeModel.move(1, 2);
-    ticTacToeModel.move(2, 2);
+    /*
+     *    X   X   O
+     *    O   O   X
+     *    X   X   O
+     */
+    assertFalse(ticTacToeModel.isGameOver());
+    ticTacToeModel.move(0, 0);// Player X moves
+    ticTacToeModel.move(1, 0);// Player O moves
+    ticTacToeModel.move(2, 0);// Player X moves
+    ticTacToeModel.move(1, 1);// Player O moves
+    ticTacToeModel.move(0, 1);// Player X moves
+    ticTacToeModel.move(0, 2);// Player O moves
+    ticTacToeModel.move(1, 2);// Player X moves
+    ticTacToeModel.move(2, 2);// Player O moves
+    ticTacToeModel.move(2, 1);// Player X moves
     assertTrue(ticTacToeModel.isGameOver());
   }
 
   @Test
-  public void getWinner() {
-    ticTacToeModel.move(0, 0);
-    ticTacToeModel.move(1, 0);
-    ticTacToeModel.move(0, 1);
-    ticTacToeModel.move(1, 1);
-    ticTacToeModel.move(0, 2);
+  public void getWinnertypical() {
+    //player X win
+    /*
+     *    O   O
+     *    X   X   X
+     *            O
+     */
+    ticTacToeModel.move(1, 1); // Player X moves
+    ticTacToeModel.move(0, 0); // Player O moves
+    ticTacToeModel.move(1, 2); // Player X moves
+    ticTacToeModel.move(0, 1); // Player O moves
+    ticTacToeModel.move(1, 0); // Player X moves
+    ticTacToeModel.move(2, 2); // Player O moves
     assertEquals(Player.X, ticTacToeModel.getWinner());
-    //tie
-    ticTacToeModel.move(0, 0);
-    ticTacToeModel.move(1, 0);
-    ticTacToeModel.move(2, 0);
-    ticTacToeModel.move(1, 1);
-    ticTacToeModel.move(0, 1);
-    ticTacToeModel.move(0, 2);
-    ticTacToeModel.move(1, 2);
-    ticTacToeModel.move(2, 2);
+
+  }
+  @Test
+  public void getWinnernull() {
+    //tie means no winner
+    /*
+     *    X   X   O
+     *    O   O   X
+     *    X   X   O
+     */
+    ticTacToeModel.move(0, 0);// Player X moves
+    ticTacToeModel.move(1, 0);// Player O moves
+    ticTacToeModel.move(2, 0);// Player X moves
+    ticTacToeModel.move(1, 1);// Player O moves
+    ticTacToeModel.move(0, 1);// Player X moves
+    ticTacToeModel.move(0, 2);// Player O moves
+    ticTacToeModel.move(1, 2);// Player X moves
+    ticTacToeModel.move(2, 2);// Player O moves
+    ticTacToeModel.move(2, 1);// Player X moves
     assertNull(ticTacToeModel.getWinner());
   }
 }
